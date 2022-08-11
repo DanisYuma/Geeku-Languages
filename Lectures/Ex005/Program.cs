97cb895c6b2a5cbfe84472de048a65edffeaf763
Danis <danyma7@gmail.com>
2022-08-11 13:23:46 +0500

    Ex005

---
+++ Lectures/Ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ведите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "danis")
+{
+    Console.WriteLine("Салам, Danis");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
