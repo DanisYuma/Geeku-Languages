e48a1517e22183a186af7bf11253f52f09b395fb
Danis <danyma7@gmail.com>
2022-08-11 16:41:43 +0500

    Seminar1/HW2

---
+++ Homework/Seminar1/HW2/Program.cs
@@ -0,0 +1,22 @@
+﻿//  На вход 3 числа, на выходе максимум из этих чисел
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  //  Считывает с консоли строку
+    int result = int.Parse(readValue);      // Преобразуют строку в целое число
+    return result;                          // Возвращает результат
+}
+
+int a;                                  // Объявление переменной
+int b;
+int c;
+a = Prompt("Enter first number>");      // Вводим значение
+b = Prompt("Enter second number>");  
+c = Prompt("Enter third number>");    
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+{
+    System.Console.WriteLine($"max = {max}");
+}
