9140f1b1eb941beb54591a0b3941d6b94120653e
Danis <danyma7@gmail.com>
2022-08-11 16:55:36 +0500

    Seminar1/HW3

---
+++ Homework/Seminar1/HW3/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Проверка четности числа
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  //  Считывает с консоли строку
+    int result = int.Parse(readValue);      // Преобразуют строку в целое число
+    return result;                          // Возвращает результат
+}
+
+int a;                                  // Объявление переменной
+a = Prompt("Enter number> ");      // Вводим значение
+if (a % 2 == 0)
+{
+    System.Console.WriteLine($"number {a} is even");
+}
+else
+{
+    System.Console.WriteLine($"number {a} is odd");
+}
