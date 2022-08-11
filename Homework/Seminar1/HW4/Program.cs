f17ba12ca86ca02642f6d65abd00d30567832894
Danis <danyma7@gmail.com>
2022-08-11 17:37:49 +0500

    Seminar1/HW4

---
+++ Homework/Seminar1/HW4/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Вывод всех четных чисел от одного до N
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  //  Считывает с консоли строку
+    int result = int.Parse(readValue);      // Преобразуют строку в целое число
+    return result;                          // Возвращает результат
+}
+
+int N = Prompt("Enter number> ");         // Вводим значение                                 
+int i = 2;                              
+while (i <= N)
+{
+    System.Console.WriteLine($"{i}");
+    i = i + 2;
+}
