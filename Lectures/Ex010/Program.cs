8aa8aebb2bd5acea97c3ed79b718c8f8fbbc3282
Danis <danyma7@gmail.com>
2022-08-14 20:36:03 +0500

    Ex010

---
+++ Lectures/Ex010/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {12,4,54,58,32,47,94,12,10,54};
+
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;                      // Оператор для прерывания операции
+    }
+    index++;
+}
