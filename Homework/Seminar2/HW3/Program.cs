8ce9848daee633dfc1f075179b9967f836b19abe
Danis <danyma7@gmail.com>
2022-08-15 20:37:51 +0500

    Seminar2 HW3

---
+++ Homework/Seminar2/HW3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа выводит третью цифру, либо сообщает что ее нету
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  //  Считывает с консоли строку
+    int result = int.Parse(readValue);      // Преобразуют строку в целое число
+    return result;                          // Возвращает результат
+}
+
+int Get3Digit(int number)
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    int third = number % 10;
+    return third;
+}
+
+int number = Prompt("Введите число>");       // Объявление переменной
+if (number > 99)
+{
+    int third = Get3Digit(number);
+    System.Console.WriteLine($"Третья цифра - {third}");
+}
+else
+{
+    System.Console.WriteLine("третьей цифры нету");
+}
