f880e7a08dd49ceb5e52d3ec388b08e9b686e416
Danis <danyma7@gmail.com>
2022-08-15 10:42:00 +0500

    Seminar2 Task1

---
+++ Seminars/Seminar2/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывод 3 цифру из трехзначного числа
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  //  Считывает с консоли строку
+    int result = int.Parse(readValue);      // Преобразуют строку в целое число
+    return result;                          // Возвращает результат
+}
+
+int x = Prompt("Введите число x> ");       // Объявление переменной
+int a = x % 10;
+System.Console.WriteLine(a);
