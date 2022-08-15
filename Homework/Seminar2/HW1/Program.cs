7470b89ab0fac060d6ea779424a5464d1520150e
Danis <danyma7@gmail.com>
2022-08-15 18:22:43 +0500

    Seminar2 HW1

---
+++ Homework/Seminar2/HW1/Program.cs
@@ -0,0 +1,6 @@
+﻿// показать вторую цифру трехзначного числа
+
+int number = new Random().Next(100, 1000);       // Объявление переменной
+int second = number/10 % 10;
+System.Console.WriteLine(number);
+System.Console.WriteLine(second);
