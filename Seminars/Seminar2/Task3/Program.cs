a9b4383aacf44dc7484a4e888ef8bb92c2f5de61
Danis <danyma7@gmail.com>
2022-08-15 11:52:29 +0500

    Seminar2 Task3

--- Seminars/Seminar2/Task3/Program.cs
+++ Seminars/Seminar2/Task3/Program.cs
@@ -1,4 +1,7 @@
-﻿// Вывод 3 цифру из трехзначного числа
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деление.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
 
 int Prompt(string message)
 {
@@ -8,10 +11,16 @@ int Prompt(string message)
     return result;                          // Возвращает результат
 }
 
-int number = new Random().Next(10, 100);       // Объявление переменной
-int a = number % 10;
-int b = number/10 % 10;
-int max = a;
-if (b>a) max = b;
-System.Console.WriteLine(number);
-System.Console.WriteLine(max);
+int number = new Random().Next(1, 10);       // Объявление переменной
+int square = new Random().Next(10, 100);
+System.Console.WriteLine($"{number}");
+System.Console.WriteLine($"{square}");
+if (square % number == 0)
+{
+    System.Console.WriteLine($"{square} делится на {number} без остатка");
+}
+else
+{
+    int mod = square % number;
+    System.Console.WriteLine($"Остаток от деления равен {mod}");
+}
