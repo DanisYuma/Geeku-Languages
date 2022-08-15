efcaf93fe20e98b83d3333da775a1e2a6f646ed7
Danis <danyma7@gmail.com>
2022-08-15 18:36:21 +0500

    Seminar2 HW2

---
+++ Homework/Seminar2/HW2/Program.cs
@@ -0,0 +1,9 @@
+﻿// удалить вторую цифру трехзначного числа и записать как двухзначное
+
+int number = new Random().Next(100, 1000);       // Объявление переменной
+int third = number % 10;
+int second = number/10 % 10;
+int first = number/100 %10;
+int TwoDigit = first * 10 + third;
+System.Console.WriteLine(number);
+System.Console.WriteLine(TwoDigit);
