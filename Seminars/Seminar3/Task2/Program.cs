e119f002c406611dabfef58ad65a0f1d88558f77
Danis <danyma7@gmail.com>
2022-08-17 18:47:34 +0500

    Seminar3 Task3

---
+++ Seminars/Seminar3/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон 
+// возможных координат точек в этой четверти (x и y).
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  //  Считывает с консоли строку
+    int result = int.Parse(readValue);      // Преобразуют строку в целое число
+    return result;                          // Возвращает результат
+}
+
+int ShowCoord(int number)
+{
+    if (number < 1 && number > 4)
+    {
+        System.Console.WriteLine("Не правильно указана четверть");
+    }
+    if (number == 1)
+    {
+       System.Console.WriteLine("X > 0 и Y > 0");
+    }
+    if (number == 2)
+    {
+        System.Console.WriteLine("X < 0 и Y > 0");
+    }
+    if (number == 3)
+    {
+        System.Console.WriteLine("X < 0 и Y < 0");
+    }
+    if (number == 4)
+    {
+        System.Console.WriteLine("X > 0 и Y < 0");
+    }
+    // else
+    // {
+    //     System.Console.WriteLine("Не правильно указана четверть");
+    // }
+}
+
+int number = Prompt("Введите номер четверти > ");
+
+int quarter = ShowCoord(number);
---
+++ Seminars/Seminar3/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  //  Считывает с консоли строку
+    int result = int.Parse(readValue);      // Преобразуют строку в целое число
+    return result;                          // Возвращает результат
+}
+
+double LineLength(int ax, int ay, int bx, int by) 
+{
+    return Math.Sqrt((ax-bx)*(ax-bx)+(by-ay)*(by-ay));
+}
+
+int ax = Prompt("Введите координату первой точки по x > ");
+int ay = Prompt("Введите координату первой точки по y > ");
+int bx = Prompt("Введите координату второй точки по x > ");
+int by = Prompt("Введите координату второй точки по y > ");
+
+double result = LineLength(ax, ay, bx, by);
+System.Console.WriteLine(result);
