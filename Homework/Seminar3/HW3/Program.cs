4cc1869119c994475f302ee88b5ba7525ec6a79b
Danis <danyma7@gmail.com>
2022-08-18 17:06:02 +0500

    Seminar3 HW3

---
+++ Homework/Seminar3/HW3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27. 
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  //  Считывает с консоли строку
+    int result = int.Parse(readValue);      // Преобразуют строку в целое число
+    return result;                          // Возвращает результат
+}
+
+void cubeTable(int num)
+{
+    for (int i =1; i <=num; i++)
+    {
+        int cube = i*i*i;
+        System.Console.Write($"{cube}" + "\t");
+    }
+}
+
+int N = Prompt("Введите N> ");
+cubeTable(N);
