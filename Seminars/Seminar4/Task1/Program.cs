9f9d2eef86de496798c6111865734196ae02a374
Danis <danyma7@gmail.com>
2022-08-21 20:59:49 +0500

    Seminar4 Task1 Task2

---
+++ Seminars/Seminar4/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    return int.Parse(Console.ReadLine());      //
+}
+
+int sumSequence(int A)
+{
+    int answer = 0;
+    for (int i = 0; i <= A; i++)
+    {
+        answer += i;
+    }
+    return answer;
+}
+
+int sumGauss (int A)
+{
+    int answer = (int)((A+1) * (A/2.0));
+    return answer;
+}
+
+int A = Prompt("Введите число A > ");
+System.Console.WriteLine($"Сумма чисел от 1 до {A} равно {sumSequence(A)}");
+System.Console.WriteLine($"Сумма чисел  по Гауссу от 1 до {A} равно {sumGauss(A)}");
---
+++ Seminars/Seminar4/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    return int.Parse(Console.ReadLine());      //
+}
+
+
+int countNumber (int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number/10;
+        count++;
+    }
+    return count;
+}
+
+int number = Prompt("Введите число > ");
+System.Console.WriteLine($"Число {number} состоит из {countNumber(number)} цифр");
