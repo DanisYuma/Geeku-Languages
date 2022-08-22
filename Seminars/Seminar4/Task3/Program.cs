94566c2f79ee4c3539117ae5836a34da6947d97d
Danis <danyma7@gmail.com>
2022-08-22 18:41:58 +0500

    Seminar4 Task3 Task4

---
+++ Seminars/Seminar4/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт произведение чисел от 1 до N.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    return int.Parse(Console.ReadLine());      //
+}
+
+double Factorial(double N)
+{
+    double answer = 1;
+    for (double i = 1; i <= N; i++)
+    {
+        answer = answer * i;
+        System.Console.WriteLine($"Факториал {i} равен {answer}");
+    }
+    return answer;
+}
+
+// int sumGauss (int A)
+// {
+//     int answer = (int)((A+1) * (A/2.0));
+//     return answer;
+// }
+
+double N = Prompt("Введите число A > ");
+System.Console.WriteLine($"Произведение чисел от 1 до {N} равно {Factorial(N)}");
+// System.Console.WriteLine($"Сумма чисел  по Гауссу от 1 до {N} равно {sumGauss(N)}");
--- Seminars/Seminar4/Task4/Program.cs
+++ Seminars/Seminar4/Task4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// int[] array = new int[8];                           // Без функции
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0, 2);             // Задание массива и печать массива в одном цикле
+//     System.Console.Write($"{array[i]}" + " ");
+// }
+
+int[] generateArray(int Length)                     // Функция 
+{
+    int[] array = new int[Length];                  // Задание массива в функции        
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void printArray (int [] array)                      // Печать массива в функции
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
+
+int [] array = generateArray(8);
+printArray (array);
