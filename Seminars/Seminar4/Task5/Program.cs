f93a713b92236ac91d5a29c537e8341148206f5c
Danis <danyma7@gmail.com>
2022-08-22 19:49:14 +0500

    Seminar4 Task5

---
+++ Seminars/Seminar4/Task5/Program.cs
@@ -0,0 +1,27 @@
+﻿// написать программу которая выводит последовательность цифр в таком формате 1 2 2 3 3 3 4 4 4 4
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    return int.Parse(Console.ReadLine());      //
+}
+
+void PrintNumbers(int N)
+{
+    int count = 0;                              // счетчик для выведенных чисел
+    for (int i = 0; i <= N; i++)                
+    {
+        for (int j = 0; j < i; j++)
+        {
+            System.Console.WriteLine($"{i} ");
+            count++;
+            if (count == N)
+            {
+                return;
+            }
+        }
+    }
+}
+
+int number = Prompt("Введите число > ");
+PrintNumbers(number);
