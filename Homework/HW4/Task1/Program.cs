b5fe06045310f3295a05f65d47fe95aa49d1f6e3
Danis <danyma7@gmail.com>
2022-08-22 20:28:30 +0500

    HW4 Task1 Task2

---
+++ Homework/HW4/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    return int.Parse(Console.ReadLine());      //
+}
+
+
+int Sqaring(int number, int multiply)
+{
+int result = 1;
+    for (int i = 0; i < multiply; i++)
+    {
+        result = result * number;
+    }
+    return result;
+}
+
+int A = Prompt("Введите A > ");
+int B = Prompt("Введите B > ");
+System.Console.WriteLine($"{A} в степени {B} равно {Sqaring(A, B)}");
---
+++ Homework/HW4/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    return int.Parse(Console.ReadLine());      //
+}
+
+
+int sumDigits(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        int digit = num %10;
+        result += digit;
+        num = num/10;
+    }
+    return result;
+}
+
+int number = Prompt("Введите число > ");
+System.Console.WriteLine($"сумма цифр числа {number} равно {sumDigits(number)}");
