0be5e5969663ba711e0f12f46c388a559046716c
Danis <danyma7@gmail.com>
2022-08-25 16:31:30 +0500

    HW5 Task1

---
+++ Homework/HW5/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] answer = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(min, max + 1);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array)            // вывод массива на консоль
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int CountEvenNumbers ( int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] %2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = CreateArray(8, 100, 1000);
+PrintArray(array);
+// int 
+System.Console.WriteLine($"Количество четных чисел в массиве - {CountEvenNumbers(array)}");
