e473ab44ac573420b7c4e73802f1796b4bd3174c
Danis <danyma7@gmail.com>
2022-08-31 20:20:03 +0500

    Seminar7 Task0

---
+++ Seminars/Seminar7/Task0/Program.cs
@@ -0,0 +1,27 @@
+﻿// Сгенерировать двумерный массив со случайными числами, вывести его на экран, оформить в виде функций
+
+int[,] FillArray(int rows, int columns, int randomMin, int randomMax)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(randomMin, randomMax);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        { Console.Write($"{matrix[i, j]} \t"); }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = FillArray(3, 4, 0, 40);
+PrintArray (matrix);
